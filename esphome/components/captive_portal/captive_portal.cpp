a3e91dfee8ce08ff60c6beafddb96fd6288a71be
Tomáš Dočkal <tomas-dockal@seznam.cz>
2024-01-18 11:45:37 +0100

    Update captive_portal.cpp

--- esphome/components/captive_portal/captive_portal.cpp
+++ esphome/components/captive_portal/captive_portal.cpp
@@ -29,7 +29,7 @@ void CaptivePortal::handle_wifisave(AsyncWebServerRequest *request) {
   std::string ssid = request->arg("ssid").c_str();
   std::string psk = request->arg("psk").c_str();
   id(ssid_text).publish_state("Hello World");
-  ESP_LOGI(TAG, "Captive Portal Requested WiFi Settings Change:");
+  ESP_LOGI(TAG, "Captive Postal Requested WiFi Settings Change:");
   ESP_LOGI(TAG, "  SSID='%s'", ssid.c_str());
   ESP_LOGI(TAG, "  Password=" LOG_SECRET("'%s'"), psk.c_str());
   wifi::global_wifi_component->save_wifi_sta(ssid, psk);
